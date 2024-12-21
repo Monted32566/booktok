6148a63a2075f4fd6bcf89eeb4ee116f36e56505
Monted32566 <misiekmichalski2005@wp.pl>
2024-12-21 14:18:56 +0100

    adding count_charcters function to program.py

--- program.py
+++ program.py
@@ -2,9 +2,21 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
         return file_content
+
 def wordcount():
     count = 0
     content = main()
     words = content.split()
-    print(len(words))
-wordcount()
+    return words
+
+def count_characters():
+    content_2 = main()
+    lowered_content = content_2.lower()
+    characters = {}
+    for letter in lowered_content:
+        if letter not in characters:
+            characters[letter] = 1
+        else:
+            characters[letter] += 1
+    print(characters)
+count_characters()
