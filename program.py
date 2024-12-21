271fa6b9298cbe27df672eee186d08779ff11101
Monted32566 <misiekmichalski2005@wp.pl>
2024-12-21 13:55:23 +0100

    adding wordcount function to program.py

--- program.py
+++ program.py
@@ -1,6 +1,10 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
-        print(file_content)
-    
-main()
+        return file_content
+def wordcount():
+    count = 0
+    content = main()
+    words = content.split()
+    print(len(words))
+wordcount()
