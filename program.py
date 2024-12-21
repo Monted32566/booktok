0a1a8b04429de0991f12278d9341da41656da04e
Monted32566 <misiekmichalski2005@wp.pl>
2024-12-21 15:07:30 +0100

    little details added

--- program.py
+++ program.py
@@ -1,16 +1,16 @@
-def main():
+def book():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
         return file_content
 
-def wordcount():
+def get_word_count():
     count = 0
-    content = main()
+    content = book()
     words = content.split()
     return words
 
 def count_characters():
-    content_2 = main()
+    content_2 = book()
     lowered_content = content_2.lower()
     characters = {}
     for letter in lowered_content:
@@ -19,8 +19,10 @@ def count_characters():
         else:
             characters[letter] += 1
     return characters
+
 def sort_on(dict):
     return dict["num"]
+
 def report():
     characters_list = []
     charac = count_characters()
@@ -29,8 +31,9 @@ def report():
             characters_list.append({"char": character, "num": charac[character]})
     characters_list.sort(reverse=True, key=sort_on)
     print("--- Begin report of books/frankenstein.txt ---")
-    print(len(wordcount()),  "words were found in the document")
+    print(len(get_word_count()),  "words were found in the document")
     for dicti in characters_list:
         print(f"The '{dicti['char']}' character was found {dicti['num']} times" )
     print("--- End report ---")
+
 report()
